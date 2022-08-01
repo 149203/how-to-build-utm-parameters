634ff2e14f616efaed4ac80aba3e18415fc622de
149203 <149203@gmail.com>
2022-08-01 00:33:29 -0400

    change react and react-dom to 17 to play nice with preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://utm-url-builder.netlify.app/`,
   },
   plugins: [`gatsby-plugin-preact`, `gatsby-plugin-sass`],
 };
